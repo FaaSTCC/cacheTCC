8e4fe368a05e13419e306a7e7242d4a40e8195e0
JRafaelSoares <joao.rafael.pinto.soares@tecnico.ulisboa.pt>
2021-10-26 18:27:08 +0100

    fix: added disclaimers to existing licenses

--- include/causal/causal_cache_utils.hpp
+++ include/causal/causal_cache_utils.hpp
@@ -11,7 +11,7 @@
 //  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
-
+//  Modifications copyright (C) 2021 Taras Lykhenko, Rafael Soares
 #ifndef INCLUDE_CAUSAL_CAUSAL_CACHE_UTILS_HPP_
 #define INCLUDE_CAUSAL_CAUSAL_CACHE_UTILS_HPP_
 
